b3c79d44a685b75f85a5e299a4b7a0d691bae1f9
Harikrishnan <haripdm9446718962@gmail.com>
2024-09-14 23:01:29 +0530

    Two sum using hash map

--- TwoSum.py
+++ TwoSum.py
@@ -13,4 +13,21 @@ result = []
 obj = solution()
 result= obj.twoSum([2,6,-6,15],9)        
 print(result)       
-        
+
+
+# Using Hash map
+
+def two_sum(arr, target):
+    prev_map = dict()  # Hash map
+
+    for i, num in enumerate(arr):
+        if target - num in prev_map:
+            return [i, prev_map[target - num]]
+        else:
+            prev_map[num] = i
+
+
+print(two_sum([1, 7, 8, 3], 4))
+
+# Time complexity: O(n)
+# Space complexity: O(n)
