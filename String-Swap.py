68cb65417524117467758042ed30c00c8ac85672
Harikrishnan <haripdm9446718962@gmail.com>
2025-02-05 21:46:22 +0530

    solved

--- String-Swap.py
+++ String-Swap.py
@@ -52,3 +52,22 @@ def areAlmostEqual(s1: str, s2: str) -> bool:
 
     # There should be exactly 2 differing positions, and swapping should fix it
     return len(diff) == 2 and diff[0] == diff[1][::-1]
+
+
+
+# Another approach
+
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1==s2:
+            return True
+        if sorted(s1)!=sorted(s2):
+            return False
+        count=0
+        for i in range(len(s1)):
+            if s1[i]!=s2[i]:
+                count+=1
+        if count!=2:
+            return False
+        return True
